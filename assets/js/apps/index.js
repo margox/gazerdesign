9aee3e02bec3ba02c659563c2534d4df68db7b35
Margox <margox@foxmail.com>
2016-01-06 16:26:22 +0800

    头部风格调整+联系方式

--- assets/js/apps/index.js
+++ assets/js/apps/index.js
@@ -12,24 +12,45 @@ require(['jquery','owlCarousel','imgloaded'],function() {
     // 固定顶栏效果
     (function() {
 
+        // return false;
+
         var navbar = $('#jNavBar');
         var navLinks = navbar.find('.primary-menu a');
+        var menuIndicator = $('#jMenuIndicator');
+        var autoScolling = false;
         var floorTops = [];
 
+        menuIndicator.css({
+            left: navLinks.eq(0).offset().left - 20,
+            width: navLinks.eq(0).width() + 70
+        });
+
         navLinks.on('click', function() {
+
             var selector = $(this).attr('data-target');
             scrollToELement(selector);
+            hightLightNavItem(navLinks.index(this));
+            navbar.addClass('fixed');
+
         }).each(function(index) {
+
             var target = $($(this).attr('data-target'));
             floorTops[index] = [
                 target.offset().top - 100,
                 target.offset().top + target.innerHeight() - 100
             ];
+
         });
 
         $(window).on('scroll', function() {
+
             var scrollTop = $(this).scrollTop();
             var nomatch = true;
+
+            if (autoScolling) {
+                return false;
+            }
+
             adjustNavBar(scrollTop);
             $.each(floorTops, function(index) {
                 if (scrollTop > this[0] && scrollTop < this[1]) {
@@ -37,46 +58,72 @@ require(['jquery','owlCarousel','imgloaded'],function() {
                     nomatch = false;
                 }
             });
+
             if (nomatch) {
                 hightLightNavItem(-1);
             }
+
         }).on('load', function() {
+
             var scrollTop = $(this).scrollTop();
             adjustNavBar(scrollTop);
+
         });
 
         function hightLightNavItem(index) {
+
+            var currentItem;
+
+            navLinks.removeClass('active');
             if (index === -1) {
-                navLinks.removeClass('active');
+                menuIndicator.stop().animate({
+                    opacity: 0
+                }, 300);
             } else {
-                navLinks.removeClass('active').eq(index).addClass('active');
+                currentItem = navLinks.removeClass('active').eq(index);
+                currentItem.addClass('active');
+                menuIndicator.stop().animate({
+                    opacity: 1,
+                    width: currentItem.width() + 70,
+                    left: currentItem.offset().left - 20
+                }, 300);
             }
+
         } 
 
         function adjustNavBar(scrollTop) {
+
             if (scrollTop > 395) {
                 navbar.addClass('fixed');
             } else {
                 navbar.removeClass('fixed');
             }
+
         }
 
         function scrollToELement(selector) {
+
             var offsetTop = 0;
             var distance = 0;
+
             if (selector) {
                 offsetTop = $(selector).offset().top || 0;
             }
+
+            autoScolling = true;
             $('html, body').stop().animate({
-                scrollTop: offsetTop - 95
-            }, 500);
+                scrollTop: offsetTop - 98
+            }, 500, function() {
+                autoScolling = false;
+            });
         }
 
     })();
 
-    // 团队成员起泡切换效果
+    // 团队成员气泡切换效果
     var _quotePops = $('#jQuotePops');
     var _avatars = $('#jAvatars');
+
     _avatars.on('click', 'a', function() {
         _quotePops.attr('data-user', $(this).attr('data-user') || 1);
         return false;
